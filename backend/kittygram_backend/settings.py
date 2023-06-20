c12a169cbd9eeef9ff7a3446fa000325c0599fcc
drukk <drukk89@gmail.com>
2023-06-20 12:51:31 +0000

    add find_dotenv

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,11 +1,11 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv(find_dotenv())
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
